0df6fa49d6094134bbb4aedf6612689b756cd67c
Szabo Richard <richard.szabo@healcloud.com>
2019-10-11 10:44:07 +0200

    Header added sub items added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
-import { Switch, Route,  } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 import "./App.css";
-
-const HatsPAge = () => (
-  <div>
-    <h1>Hats PAge</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-         <Route exact path="/" component={HomePage} />
-         <Route path='/hats' component={HatsPAge} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
