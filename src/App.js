854160e8c1bfc9b2d4ffa954cc93afdc9ea31efe
Szabo Richard <richard.szabo@healcloud.com>
2019-10-15 15:56:26 +0200

    redux added

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     };
   }
 
-  componentDidMount() {
+  componentDidMount() { 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
