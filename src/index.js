e8593cec84130015c1c45dcfde5fb7550ce38300
Szabo Richard <richard.szabo@healcloud.com>
2019-10-22 13:37:32 +0200

    redux-persist added

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 import App from "./App";
 
-import store from './redux/store';
+import store from "./redux/store";
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={store.store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={store.persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
