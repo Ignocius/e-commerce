1e50dd74d2826dddc718ed1d058e9680a8df2c26
Szabo Richard <richard.szabo@healcloud.com>
2019-10-28 16:46:19 +0100

    redux thunk added logic rewritten from shop

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,31 @@
 import ShopActionTypes  from './shop.types';
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTION_START
+})
+
+export const fetchCollectionSucces = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTION_SUCCESS,
     payload: collectionsMap
 })
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTION_FAILURE,
+    payload: errorMessage
+})
+
+export const fetchCollectionsStartAsync = () => {
+    console.log('called')
+    return dispatch => {
+        const collectionRef = firestore.collection('collection');
+        dispatch(fetchCollectionsStart());
+        
+
+        collectionRef.get().then(snapshot => {
+            const collectionMap = convertCollectionSnapshotToMap(snapshot);
+            dispatch(fetchCollectionSucces(collectionMap));
+        }).catch(error => dispatch(fetchCollectionsFailure(error)));
+    }
+}
